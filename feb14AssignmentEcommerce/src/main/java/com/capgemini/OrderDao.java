b93482c5683e08d0afdc170a4eb73776327852d5
Mohammad Yusuf <118352964+mohd1612yusuf@users.noreply.github.com>
2026-02-16 09:33:45 +0530

    Fix database connection string in OrderDao

--- feb14AssignmentEcommerce/src/main/java/com/capgemini/OrderDao.java
+++ feb14AssignmentEcommerce/src/main/java/com/capgemini/OrderDao.java
@@ -9,7 +9,7 @@ import javax.persistence.Persistence;
 
 public class OrderDao {
 
-	EntityManagerFactory emf = Persistence.createEntityManagerFactory("postgresql");
+	EntityManagerFactory emf = Persistence.createEntityManagerFactory("postgres");
 	EntityManager em = emf.createEntityManager();
 	EntityTransaction et = em.getTransaction();
 
